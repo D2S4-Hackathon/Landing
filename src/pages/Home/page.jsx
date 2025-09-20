d4a4f1a7b96f4bff9cbd008a970a6545e73d37c9
kdhdd <kdso10@naver.com>
2025-09-21 03:20:02 +0900

    :sparkles: Feat: background 추가

--- src/pages/Home/page.jsx
+++ src/pages/Home/page.jsx
@@ -1,31 +1,150 @@
 import styled from "styled-components";
-import { Header } from "./components/Header";
+import backgroundImage from "../../assets/images/background.png";
 
 export default function Home() {
   return (
     <Layout>
-      <Header />
-      <p>그동안 배운걸 바탕으로 드디어 결실을 거둘 때가 왔습니다.</p>
-      <p>본걱젹으로 팀으로 하는 프로젝트인만큼 추천드리는 초기 세팅을 예시로 준비해봤습니다.</p>
-      <p>
-        그대로 따라하는 것 보다는, 본인 팀만의 폴더 구조나 파일 이름같은 것들을 개발 시작 전에
-        충분히 얘기해서 정해놓고 하는걸 추천드립니다.
-        <br />
-        (그래야 나중에 깃허브 충돌이 안나요..!)
-      </p>
-      <p>
-        열심히 달려온 여러분에게 주어지는 흔치 않은 기회이니, 이 기회 놓치지 마시고 한달동안 조금만
-        더 힘내서 해커톤 잘 마무리합시다!!!
-      </p>
-      <p>기술적이거나 기술 외적인 부분으로 도움이 필요하면 언제든지 멘토에게 연락주세요 :)</p>
+      <MainContainer>
+        <BackgroundImage />
+        <WhiteLine />
+        <BackgroundText>Background</BackgroundText>
+        <MainTitle>정보의 바다 앞에서, 누군가는 늘 멈춰야 했습니다.</MainTitle>
+        <MainParagraph>
+          <ParagraphSection>
+            우리 주변에는 언제나 조금 더 오래 걸리는 사람들이 있습니다. <br />
+            누구보다 먼저 움직이고 싶지만, 늘 한 발 늦게 도착할 수밖에 없는 사람들.
+          </ParagraphSection>
+          <ParagraphSection>
+            세상이 쏟아내는 정보의 늪 속에서 그들은 원하는 답 하나를 얻기 위해 <br />
+            수많은 단계를 거치고, 불필요한 길을 돌아야만 합니다. <br />
+            그들은 바로 시각장애인입니다.
+          </ParagraphSection>
+          <ParagraphSection>
+            검색 한 번, 클릭 한 번이 우리에게는 당연한 일상이지만, <br />
+            그들에게는 끝이 없을 것 같은 고단한 여정이 됩니다.
+          </ParagraphSection>
+          <SubParagraph>그리고 그 여정 끝에 그들은 이렇게 묻습니다.</SubParagraph>
+        </MainParagraph>
+        <QuestionText>"왜 우리는 정보를 찾는 것조차 이렇게 힘들어야 합니까?"</QuestionText>
+      </MainContainer>
     </Layout>
   );
 }
 
 const Layout = styled.div`
-  padding: 1rem;
+  padding: 0;
+  margin: 0;
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 1rem;
+`;
+
+const MainContainer = styled.div`
+  position: relative;
+  width: 1920px;
+  height: 1080px;
+  background: #ffffff;
+  overflow: hidden;
+`;
+
+const BackgroundImage = styled.div`
+  position: absolute;
+  width: 1920px;
+  height: 1080px;
+  left: 0px;
+  top: 0px;
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${backgroundImage});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+const WhiteLine = styled.div`
+  position: absolute;
+  width: 22px;
+  height: 5px;
+  left: 950px;
+  top: 8.5rem;
+  background: #ffffff;
+`;
+
+const BackgroundText = styled.div`
+  position: absolute;
+  width: 1289px;
+  height: 42px;
+  left: calc(50% - 1289px / 2 + 0.5px);
+  top: 147px;
+  font-family: "AppleSDGothicNeoM00", -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 27px;
+  line-height: 42px;
+  text-align: center;
+  color: #ffffff;
+`;
+
+const MainTitle = styled.div`
+  position: absolute;
+  width: 1006px;
+  height: 82px;
+  left: calc(50% - 1006px / 2);
+  top: 195px;
+  font-family: "AppleSDGothicNeoH00", -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 51.8529px;
+  line-height: 82px;
+  text-align: center;
+  color: #ffffff;
+  white-space: nowrap;
+`;
+
+const MainParagraph = styled.div`
+  position: absolute;
+  width: 1289px;
+  height: 410px;
+  left: calc(50% - 1289px / 2 + 0.5px);
+  top: 401px;
+  font-family: "AppleSDGothicNeoM00", -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 42px;
+  text-align: center;
+  color: #ffffff;
+`;
+
+const SubParagraph = styled.div`
+  position: absolute;
+  width: 1289px;
+  height: 410px;
+  left: calc(50% - 1289px / 2 + 0.5px);
+  top: 23.5rem;
+  font-family: "AppleSDGothicNeoM00", -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 42px;
+  text-align: center;
+  color: #ffffff;
+`;
+
+const ParagraphSection = styled.div`
+  margin-bottom: 1.5rem;
+`;
+
+const QuestionText = styled.div`
+  position: absolute;
+  width: 855px;
+  height: 61px;
+  left: calc(50% - 855px / 2 + 0.3px);
+  top: 865px;
+  font-family: "AppleSDGothicNeoH00", -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 38.27px;
+  line-height: 61px;
+  text-align: center;
+  color: #00eeff;
+  white-space: nowrap;
 `;
